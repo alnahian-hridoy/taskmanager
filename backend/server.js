c6c2fcd0da364778d651ca324bf4fc7621cad161
alnahian-hridoy <alnahian.hridoy@connect.qut.edu.au>
2026-05-29 16:20:11 +1000

    Load backend .env explicitly with path.join(__dirname, '.env')

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 
 const express = require('express');
+const path = require('path');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
 
-dotenv.config();
+dotenv.config({ path: path.join(__dirname, '.env') });
 
 
 const app = express();
